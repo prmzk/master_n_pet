ab185a7ff9781303950c6fb1ce847457c6a7ab46
prmzk <adam.primarizki@gmail.com>
2020-02-26 16:51:47 +0700

    routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 const router = require('express').Router()
 const adminRouter = require('./adminRoutes')
 
-router.get('/', (req, res) => res.send('home'))
+router.get('/', (req, res) => res.render('home.ejs'))
 router.use('/admin', adminRouter)
 
 module.exports = router;

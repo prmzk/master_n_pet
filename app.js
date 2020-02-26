48823014cce84f597372badfcc25888d5ce2ec04
thomasmartuaps <thomasmartuaps@gmail.com>
2020-02-26 16:30:49 +0700

    aoapk

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express();
 const PORT = 3000
+const routers =
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/views/public"));
 app.use(express.urlencoded({extended: true}));
+
+app.use('/', routers)
 
 app.listen(PORT, ()=>console.log('Running on port', PORT))
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,5 @@
 const { User } = require('../models/index.js');
+
+cons
+
+module.exports = Controller;

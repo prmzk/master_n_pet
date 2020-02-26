9fc582e35cded2ae17b911ede48d43d515e4f5d3
thomasmartuaps <thomasmartuaps@gmail.com>
2020-02-26 16:26:24 +0700

    Thomas added seeder and ejs view engine

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const express = require('express')
 const app = express();
 const PORT = 3000
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/views/public"));
+app.use(express.urlencoded({extended: true}));
+
 app.listen(PORT, ()=>console.log('Running on port', PORT))
 

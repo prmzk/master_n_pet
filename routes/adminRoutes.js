4d7e54c01537a5b77f250d232db3b068e11aa7ad
prmzk <adam.primarizki@gmail.com>
2020-02-26 16:48:41 +0700

    admin login

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,5 +4,6 @@ const router = require('express').Router()
 const Controller = require('../controllers/adminController')
 
 router.get('/login', Controller.loginShow)
+router.post('/login', Controller.login)
 
 module.exports = router;

f229d0ad640402add0cc70e4c2150e4d33a5b2ab
thomasmartuaps <thomasmartuaps@gmail.com>
2020-02-27 22:51:53 +0700

    deploying v2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 4000
 const session = require('express-session')
 const router = require('./routes/index.js')
 

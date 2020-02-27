e5c8db6773974e0156d497da2be878b9ec84f2dd
thomasmartuaps <thomasmartuaps@gmail.com>
2020-02-27 18:04:50 +0700

    added card & profile

--- controllers/petController.js
+++ controllers/petController.js
@@ -18,7 +18,7 @@ class Controller {
             let success = req.app.locals.success
             delete req.app.locals.success
             console.log(result)
-            res.render('pets.ejs', {success:success, data:result, reqsession: req.session})
+            res.render('pets.ejs', {success:success, data:result})
         })
     }
 
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,13 @@ const petRouter = require('./petRoutes')
 
 router.get('/', (req, res) => {
     let user = req.session.user
+    let admin = req.session.admin
     if(user){
         console.log(user)
         console.log(user.name)
     }
     // res.send(user);
-    res.render('home.ejs', {user: user})
+    res.render('home.ejs', {user: user, admin: admin})
 })
 router.use('/admin', adminRouter)
 router.use('/user', userRouter)
